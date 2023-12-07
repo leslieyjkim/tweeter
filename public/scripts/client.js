024ecb0c622140e1296b7372e24a06aec9ebe3c9
yoonjookim <yjblossom@gmail.com>
2023-12-07 16:49:00 -0500

    Add empty tweetText after submitting. Successully worke.d

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@MadameIsabella",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1701728198796,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1701814598796,
-  },
-];
 
 // Helper function to convert timestamp to "time ago" format
 const timeAgo = function (timestamp) {
@@ -50,7 +26,6 @@ const timeAgo = function (timestamp) {
   }
 };
 // ---------------------------------------Mark-up Tweet----------
-// access user objects from renderTweets and accesses speific key:pair values and adds them to html template and returns back to renderTweets
 // updated 'userTweets' into '$tweet'
 const createTweetElement = (tweetData) => {
   const $tweet = $(`
@@ -79,16 +54,16 @@ const createTweetElement = (tweetData) => {
   return $tweet;
 };
 
-// -------------------------------Append new tweet to id='tweet-container'in index.html----
+// ----------Prepend(top to bottom) new tweet to id='tweet-container'in index.html----
 const renderTweets = function (tweets) {
   for (const tweet of tweets) {
     const $tweet = createTweetElement(tweet);
-    $("#tweets-container").append($tweet);
+    $("#tweets-container").prepend($tweet);
   }
 };
 
-// -------------------------------Fetching tweets from /tweets routes.
-//client-side JavaScript will use AJAX to request fetch (GET) data from the server.
+// -----------Fetching tweets from /tweets routes.
+//client-side JS uses AJAX to request fetch (GET) data from the server.
 //load tweet from "/tweets", receive array in json.
 const loadTweets = function () {
   return $.ajax({
@@ -98,39 +73,29 @@ const loadTweets = function () {
   });
 };
 
-//---------------------------------Reset function when it's under error
-const formReset = function () {
-  $("#too-long-error").hide();
-  $("#empty-error").hide();
-};
-formReset();
-
-// -----------------------------------//load tweet and render
+// -----------------------------------//loading and rendering
 $(document).ready(function () {
   loadTweets()
     .then((tweets) => {
       renderTweets(tweets);
     })
     .catch(function (error) {
-      console.error("Error loading tweets:", error);
+      console.error("Error in loading tweets:", error);
     });
 
   // event listener :  Post tweet details to server
   $("#tweet-form").on("submit", function (event) {
     event.preventDefault();
-    formReset();
     //validation
     const $tweetText = $("#tweet-text");
     const tweetContent = $tweetText.val().trim();
 
-    if (!tweetContent) {
-      // alert("Error: Tweet content cannot be empty.");
-      $("#empty-error").show();
+    if (tweetContent === "") {
+      alert("Error: Tweet content cannot be empty.");
       return;
     }
     if (tweetContent.length > 140) {
-      // alert("Error: Tweet content exceeds 140 characters.");
-      $("#too-long-error").show();
+      alert("Error: Tweet content exceeds 140 characters.");
       return;
     }
     //In case of 'No error', sending tweet to server
@@ -143,14 +108,15 @@ $(document).ready(function () {
       success: function (response) {
         console.log("Data submitted successfully!");
         // Update the UI here to show the new tweet
-        loadTweets().then((tweets) => {
-          renderTweets(tweets);
-        });
-        $("#tweet-list").prepend(response); // This adds the new tweet to the top of the list
-        console.log("Data submitted successfully!");
-      },
-      error: function (xhr, status, error) {
-        console.log("Error submitting data: " + error);
+        loadTweets()
+          .then((tweets) => {
+            renderTweets(tweets);
+          })
+          .catch(function (error) {
+            console.error("Error in submitting tweets:", error);
+          });
+        // alert("Your tweet was submitted succeessfully, tweet, tweet!");
+        $tweetText.val("");
       },
     });
   });
